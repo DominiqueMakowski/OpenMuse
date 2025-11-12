4f5159e9abf7e60389056302d8172fc1d8276a75
OliverACollins <oliver7collins@outlook.com>
2025-11-12 15:46:02 +0000

    Update view.py

--- OpenMuse/view.py
+++ OpenMuse/view.py
@@ -641,8 +641,8 @@ class RealtimeViewer:
         # Place the battery text using normalized -> pixel conversion
         # Align battery text just above the bar, same right offset
         bar = self._battery_rect_px
-        bx = bar["x"] + bar["w"] / 2
-        by = bar["y"] + bar["h"] + (0.005 * height)  # vertical gap ~1.5% of height
+        bx = (bar["x"] + bar["w"] / 2) / width
+        by = (bar["y"] + bar["h"] + (0.015 * height)) / height
         self.battery_text.pos = (bx, by)
 
 
